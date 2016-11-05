71b6104ba0b73ddaf93e25fa16b9ecba42223860
Faruk <ofk1991@gmail.com>
2016-11-05 21:56:21 +0530

    Logic for sum is added for the first 10 numbers

--- HelloWorldGit/src/main/java/test/omar/git/App.java
+++ HelloWorldGit/src/main/java/test/omar/git/App.java
@@ -9,5 +9,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int sum = 0;
+        for( int i=1; i<10; i++ )
+        {
+        	sum += i;
+        	
+        }
+        System.out.println("SUM is : " + sum);
     }
 }
